3848fdce0e4a55d7fc35f358b0db8720c12d2b86
Mizan Syed <mizansyed@gmail.com>
2013-08-04 15:52:35 +0100

    Github release

---
+++ jquery.wd_syncscroll.js
@@ -0,0 +1,45 @@
+/* jQuery SyncScroll
+ *
+ *
+ * Copyright 2013, Mizan Syed, With Digital
+ * Licensed under the Apache License, Version 2.0
+ * Read licence.txt and notice.txt for info
+ */
+
+
+;(function($) {
+	$.fn.wd_syncscroll = function(options) {
+		var defaults = {
+			overflow: 'auto',
+			height: false,
+			width: false,
+			group: 'wd'
+		};
+
+		var settings = $.extend(true, {}, defaults, options);
+
+		var stylise = function(el) {
+			el.addClass(settings.group);
+			el.css('overflow', 'auto');
+			if (settings.height !== false) {
+				el.css('max-height', settings.height);
+				el.css('height', 'auto !important');
+				el.css('height', settings.height);
+			}
+			if (settings.width !== false)
+				el.css('width', settings.width);
+		};
+
+		return this.each(function() {
+			var group = '.' + settings.group;
+			var obj = $(this);
+			stylise(obj);
+			obj.scroll(function() {
+				$(group).scrollTop(obj.scrollTop());
+				$(group).scrollLeft(obj.scrollLeft());
+			});
+
+		});
+
+	}
+})(jQuery);
